e27e8ca32cc075ce694382b0e29364c1372adb4c
Simo Ala-Kotila <simo.alakotila@gmail.com>
2020-02-09 15:05:44 +0200

    fix the main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
-	"github.com/pepeunlimited/microservice-kit/headers"
+	"github.com/pepeunlimited/billing/internal/pkg/ent"
+	"github.com/pepeunlimited/billing/internal/server/twirp"
+	"github.com/pepeunlimited/billing/pkg/orderrpc"
+	"github.com/pepeunlimited/billing/pkg/paymentrpc"
 	"github.com/pepeunlimited/microservice-kit/middleware"
-	"github.com/pepeunlimited/rpc-starter-kit/pkg/todorpc"
 	"log"
 	"net/http"
 )
@@ -17,12 +19,15 @@ func init() {
 }
 
 func main() {
-	log.Printf("Starting the TodoServer... version=[%v]", Version)
+	log.Printf("Starting the BillingSever... version=[%v]", Version)
 
-	ts := todorpc.NewTodoServiceServer(twirp.NewTodoServer(), nil)
+	client := ent.NewEntClient()
+	os := orderrpc.NewOrderServiceServer(twirp.NewOrderServer(client), nil)
+	ps := paymentrpc.NewPaymentServiceServer(twirp.NewPaymentServer(client), nil)
 
 	mux := http.NewServeMux()
-	mux.Handle(ts.PathPrefix(), middleware.Adapt(ts, headers.Username()))
+	mux.Handle(os.PathPrefix(), middleware.Adapt(os))
+	mux.Handle(ps.PathPrefix(), middleware.Adapt(ps))
 
 	if err := http.ListenAndServe(":8080", mux); err != nil {
 		log.Panic(err)
